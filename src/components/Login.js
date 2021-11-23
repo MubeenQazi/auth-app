cf81053cbeb5f12dbc322e2b82ac8004a63a36c4
MubeenQazi <mubeenqazi32@gmail.com>
2021-11-23 19:54:12 +0500

    feat(app): Make reusable Button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form";
-
+import Button from './UI/Button'
 
 const bg_layout = 'min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-r from-green-400 to-blue-500  h-screen'
 
@@ -53,13 +53,7 @@ export default function Login() {
                     <input {...register("password")} id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
                 </div>
 
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-                        <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                            Remember me
-                        </label>
-                    </div>
+                <div className="flex items-center justify-center">
 
                     <div className="text-sm">
                         <Link to='/signup' className="font-medium text-indigo-600 hover:text-indigo-500">
@@ -69,15 +63,7 @@ export default function Login() {
                 </div>
 
                 <div>
-                    <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-
-                            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                            </svg>
-                        </span>
-                        Sign in
-                    </button>
+                    <Button title="Sign In" />
                 </div>
             </form>
             {doesExits ? <p className="text-red-800">Enter correct username or password</p> : null}
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form";
+import Button from './UI/Button'
 
 
 const bg_layout = 'min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-r from-green-400 to-blue-500  h-screen'
@@ -38,7 +39,7 @@ export default function SignUp() {
     return <div className={bg_layout}>
         <div className="max-w-md w-full space-y-8 shadow-lg p-9 bg-white rounded">
             <div>
-                <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
+                <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 ">
                     Sign Up
                 </h2>
             </div>
@@ -57,7 +58,7 @@ export default function SignUp() {
                     <input {...register("password")} name="password" type="password" autoComplete="current-password" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
                 </div>
 
-                <div className="flex items-center justify-end">
+                <div className="flex items-center justify-center">
                     <div className="text-sm">
                         <Link to='/login' className="font-medium text-indigo-600 hover:text-indigo-500">
                             Already have account? Signin
@@ -66,15 +67,7 @@ export default function SignUp() {
                 </div>
 
                 <div>
-                    <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-
-                            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                            </svg>
-                        </span>
-                        Sign in
-                    </button>
+                    <Button title="Sign Up" />
                 </div>
                 {doesExits ? <p className="text-red-800">This username Already exist</p> : null}
             </form>
